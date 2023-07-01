8e370e0043b84f84ab2f1fce0cb02ab9ea63908a
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-06-30 23:03:54 -0300

    feat: make system version control

---
+++ app/version.py
@@ -0,0 +1,9 @@
+from io import open
+
+read_version = lambda filename: open(filename, "r", encoding="utf-8").read()
+
+""" It defines the project current version """
+__version__ = read_version(".version").strip() if read_version(".version") else "0.0.0"
+
+if __name__ == "__main__":
+    print(__version__)
