20485975282b9f31dc6191dcd1e4ec96cbd56cf8
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-07-03 09:10:46 -0300

    refactor: add background task from user create router.

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -7,11 +7,9 @@ from fastapi.responses import JSONResponse
 from models.users import (
     User,
     UserCreateRequest,
-    UserResponse,
-    StatusEnum,
 )
 
-from database.controllers import UserController
+from database.controllers import create_user as UserController
 
 logger = logging.getLogger("UserLogger")
 
@@ -19,25 +17,30 @@ logger = logging.getLogger("UserLogger")
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.post("/users/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserCreateRequest, background_tasks: BackgroundTasks):
+@user_router.post("/users/", status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreateRequest, background_tasks: BackgroundTasks):
     """
-    Create user\n
-    Args:\n
-        user (UserCreateRequest): User data\n
-        background_tasks (BackgroundTasks): Background tasks\n
-    Returns:\n
-        UserResponse: User data
+    Create user endpoint:
+
+    - **first_name**: the user first name(str)
+    - **last_name**: the user last name(str)
+    - **email**: the user email(valid email str)
+    - **password**: the user password(str > 8 length)
+    - **confirm_password**: the user password confirmation(str > 8 length)
+    - **phone**: the user phone(str)
+    - **documents**: the user documents(list)
+    - **address**: the user address(list)
+    - **accounts**: the user accounts(list)
+
+    Returns:
+    - **UserResponse** (User): User data to be created.
+    - **AccountResponse** (UserBankAccount): User bank account data to be created.
     """
     logger.info(f"Create user {user.email}")
 
-    user_controller = UserController(user)
-
-    user_controller.create_user()
+    background_tasks.add_task(UserController, user)
 
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
-        content=jsonable_encoder(
-            UserResponse(user.dict())
-        )  
+        content="Please check your email to confirm your account."
     )
