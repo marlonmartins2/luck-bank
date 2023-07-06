2c48c1392e145c8c6e5ef78d0929fba71da1d35d
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-07-06 19:29:29 -0300

    feat: add patch and delete routers.

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -4,16 +4,16 @@ from fastapi import APIRouter, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
-from models.users.users_model import UserCreateRequest
+from models.users.users_model import UserCreateRequest, UserUpdateRequest
 
-from database.controllers.user import create_user_model
-from database.controllers.user import get_user_by_id
+from database.controllers.user import create_user_model, get_user_by_id, update_user_model, delete_user_by_id
+
 
 
 logger = Logger.init("UserRouteLogger")
 
 
-user_router = APIRouter(prefix="/user", tags=["User"])
+user_router = APIRouter(tags=["User"])
 
 
 @user_router.post("/users/", status_code=status.HTTP_201_CREATED)
@@ -62,4 +62,48 @@ def get_user(user_id: str):
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content=jsonable_encoder(user)
+    )
+
+
+@user_router.patch("/users/{user_id}/", status_code=status.HTTP_200_OK)
+def update_user(user_id: str, payload: UserUpdateRequest):
+    """
+    Update user endpoint:
+
+    - **user_id**: the user id(str)
+    - **payload**: the user payload to update(UserUpdateRequest)
+    
+    Returns:
+    - **UserResponse** (User): User data to database return.
+    """
+    logger.info(f"Update user {user_id}")
+
+    update_user_model(user_id, payload)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "message": "User updated successfully.",
+            "payload": jsonable_encoder(payload),
+        }
+    )
+
+
+@user_router.delete("/users/{user_id}/", status_code=status.HTTP_200_OK)
+def delete_user(user_id: str):
+    """
+    Delete user endpoint:
+
+    - **user_id**: the user id(str)
+    
+    Returns:
+    - Message to user deleted successfully.
+    """
+    logger.info(f"Delete user {user_id}")
+
+    delete_user_by_id(user_id)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"message": "User deleted successfully.",}
     )
