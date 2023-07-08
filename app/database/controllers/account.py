b86840a835c628f06abff45a2c464e4f963acd56
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-07-08 01:23:52 -0300

    feat: include CRUD from account in user router.

--- app/database/controllers/account.py
+++ app/database/controllers/account.py
@@ -1,8 +1,7 @@
-from utils.logger import Logger
+from datetime import datetime
 
-from database import database, Collections
-
-from models.users.users_bank_account_model import BankAccount
+from fastapi import HTTPException, status
+from fastapi.encoders import jsonable_encoder
 
 from pymongo.errors import (
     ServerSelectionTimeoutError,
@@ -10,6 +9,13 @@ from pymongo.errors import (
     OperationFailure,
     PyMongoError
 )
+
+from utils import AccountNotSalary
+from utils.logger import Logger
+
+from database import database, Collections
+
+from models.users.users_bank_account_model import BankAccount
 
 logger = Logger.init("AccountControllerLogger")
 
@@ -22,12 +28,29 @@ def create_account(user_id, account):
         account (UserBankAccount): The account data.
     """
     logger.info(f"Create account from user_id ->: {user_id}")
-    account = BankAccount(
-        user_id=user_id,
-        account_type=account.account_type
-    ).dict()
+    try:
+        account = BankAccount(
+            user_id=user_id,
+            account_type=account.account_type
+        ).dict()
 
-    database[Collections.USER_BANK_ACCOUNTS].insert_one(account)
+        database[Collections.USER_BANK_ACCOUNTS].insert_one(account)
+
+        account.pop("_id")
+
+        return account
+
+    except ServerSelectionTimeoutError as error:
+        logger.error(f"ServerSelectionTimeoutError: {error}")
+
+    except ConnectionFailure as error:
+        logger.error(f"ConnectionFailure: {error}")
+
+    except OperationFailure as error:
+        logger.error(f"OperationFailure: {error}")
+
+    except PyMongoError as error:
+        logger.error(f"PyMongoError: {error}")
 
 
 def get_accounts_per_user(user_id):
@@ -39,19 +62,150 @@ def get_accounts_per_user(user_id):
     logger.info(f"Get accounts per user_id ->: {user_id}")
     payload = []
     try:
-        accounts = database[Collections.USER_BANK_ACCOUNTS].find({"user_id": user_id}, {"_id": 0})
+        accounts = database[Collections.USER_BANK_ACCOUNTS].find(
+            {"user_id": user_id, "deleted_at": ""},
+            {"_id": 0}
+        )
 
         for account in accounts:
             payload.append(account)
         return payload
+
     except ServerSelectionTimeoutError as error:
-        logger.debug(f"ServerSelectionTimeoutError: {error}")
+        logger.error(f"ServerSelectionTimeoutError: {error}")
 
     except ConnectionFailure as error:
-        logger.debug(f"ConnectionFailure: {error}")
+        logger.error(f"ConnectionFailure: {error}")
 
     except OperationFailure as error:
-        logger.debug(f"OperationFailure: {error}")
+        logger.error(f"OperationFailure: {error}")
 
     except PyMongoError as error:
-        logger.debug(f"PyMongoError: {error}")
+        logger.error(f"PyMongoError: {error}")
+
+
+def get_account_by_id(user_id , account_id):
+    """
+    Get account by id.
+    Args:
+        account_id (str): The account id.
+    """
+    logger.info(f"Get account detail user id: ->: {user_id}")
+
+    try:
+        account = database[Collections.USER_BANK_ACCOUNTS].find_one(
+            {"id": account_id, "user_id": user_id},
+            {"_id": 0}
+        )
+
+        if not account:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={
+                    "message": "Account not found",
+                }
+            )
+
+        return account
+
+    except ServerSelectionTimeoutError as error:
+        logger.error(f"ServerSelectionTimeoutError: {error}")
+
+    except ConnectionFailure as error:
+        logger.error(f"ConnectionFailure: {error}")
+
+    except OperationFailure as error:
+        logger.error(f"OperationFailure: {error}")
+
+    except PyMongoError as error:
+        logger.error(f"PyMongoError: {error}")
+
+
+def update_account_type(user_id, account_id, payload):
+    """
+    Update account per user.
+    Args:
+        user_id (str): The user id.
+        account (account): The account data.
+    """
+    logger.info(f"Update account per user_id: ->: {user_id}")
+
+    try:
+
+        account = database[Collections.USER_BANK_ACCOUNTS].find_one(
+            {"id": account_id},
+            {"_id": 0}
+        )
+
+        if not account:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={
+                    "message": "Address not found",
+                }
+            )
+
+        if account and account["account_type"] != "salary":
+            raise AccountNotSalary("Account is not salary")
+
+        account = payload.dict()
+        account["updated_at"] = datetime.now()
+
+        database[Collections.USER_BANK_ACCOUNTS].update_one(
+            {"id": account_id},
+            {"$set": account}
+        )
+
+
+        return account
+
+    except AccountNotSalary:
+        logger.error("Account is not salary")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "message": "Changing account type is only allowed for salary type accounts."
+                    "Please create another account.",
+            }
+        )
+
+    except ServerSelectionTimeoutError as error:
+        logger.error(f"ServerSelectionTimeoutError: {error}")
+
+    except ConnectionFailure as error:
+        logger.error(f"ConnectionFailure: {error}")
+
+    except OperationFailure as error:
+        logger.error(f"OperationFailure: {error}")
+
+    except PyMongoError as error:
+        logger.error(f"PyMongoError: {error}")
+
+
+def delete_account(account_id, user_id):
+    """
+    Delete account per user.
+    Args:
+        account_id (str): The account id.
+    """
+    logger.info(f"Delete account per user id: ->: {user_id}")
+
+    try:
+        database[Collections.USER_BANK_ACCOUNTS].update_one(
+            {"id": account_id},
+            {"$set": {"deleted_at": datetime.now()}}
+        )
+
+        return True
+
+    except ServerSelectionTimeoutError as error:
+        logger.error(f"ServerSelectionTimeoutError: {error}")
+
+    except ConnectionFailure as error:
+        logger.error(f"ConnectionFailure: {error}")
+
+    except OperationFailure as error:
+        logger.error(f"OperationFailure: {error}")
+
+    except PyMongoError as error:
+        logger.error(f"PyMongoError: {error}")
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -6,6 +6,7 @@ from fastapi.responses import JSONResponse
 
 from models.users.users_model import UserUpdateRequest
 from models.users.users_address_model import AddressCreateRequest, AddressUpdateRequest
+from models.users.users_bank_account_model import BankAccountCreateRequest
 
 from services.oauth2 import require_user, AuthJWT
 
@@ -23,6 +24,14 @@ from database.controllers.address import (
     update_address,
     delete_address,
 
+)
+
+from database.controllers.account import (
+    create_account,
+    get_accounts_per_user,
+    get_account_by_id,
+    update_account_type,
+    delete_account,
 )
 
 
@@ -217,4 +226,108 @@ def delete_user_address(user_id: str, address_id: str):
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={"message": "User address deleted successfully.",}
+    )
+
+
+@user_router.get("/users/{user_id}/accounts/", status_code=status.HTTP_200_OK)
+def get_user_accounts(user_id: str):
+    """
+    Get user accounts endpoint:
+
+    Returns:
+    - **AccountResponse** (Account): Account data to database return.
+    """
+    logger.info(f"Get user accounts")
+
+    accounts = get_accounts_per_user(user_id)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder(accounts)
+    )
+
+
+@user_router.get("/users/{user_id}/accounts/{account_id}/", status_code=status.HTTP_200_OK)
+def get_user_account_detail(user_id: str, account_id: str):
+    """
+    Get user account detail endpoint:
+
+    Returns:
+    - **AccountResponse** (Account): Account data to database return.
+    """
+    logger.info(f"Get user account detail")
+
+    account = get_account_by_id(user_id, account_id)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=jsonable_encoder(account)
+    )
+
+
+@user_router.post("/users/{user_id}/accounts/", status_code=status.HTTP_201_CREATED)
+def create_user_account(user_id: str, payload: BankAccountCreateRequest):
+    """
+    Create user account endpoint:
+
+    - **user_id**: the user id(str)
+    - **payload**: the user payload to create(BankAccountCreateRequest)
+    
+    Returns:
+    - **AccountResponse** (Account): Account data to database return.
+    """
+    logger.info(f"Create user account")
+
+    new_account = create_account(user_id, payload)
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "message": "User account created successfully.",
+            "payload": jsonable_encoder(new_account),
+        }
+    )
+
+
+@user_router.patch("/users/{user_id}/accounts/{account_id}/", status_code=status.HTTP_200_OK)
+def update_user_account(user_id: str, account_id: str, payload: BankAccountCreateRequest):
+    """
+    Update user account endpoint:
+
+    - **user_id**: the user id(str)
+    - **payload**: the user payload to update(BankAccountCreateRequest)
+    
+    Returns:
+    - **AccountResponse** (Account): Account data to database return.
+    """
+    logger.info(f"Update user account")
+
+    updated_account = update_account_type(user_id, account_id, payload)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "message": "User account updated successfully.",
+            "payload": jsonable_encoder(updated_account),
+        }
+    )
+
+
+@user_router.delete("/users/{user_id}/accounts/{account_id}/", status_code=status.HTTP_200_OK)
+def delete_user_account(user_id: str, account_id: str):
+    """
+    Delete user account endpoint:
+
+    - **user_id**: the user id(str)
+    
+    Returns:
+    - Message to user account deleted successfully.
+    """
+    logger.info(f"Delete user account")
+
+    delete_account(account_id, user_id)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"message": "User account deleted successfully.",}
     )
